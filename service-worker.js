910fa7a38dfe45ff274f48369eea23ebabbd06d8
nicole931103 <nnico931103@gmail.com>
2024-10-19 02:35:26 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,14 @@
 const CACHE_NAME = "device-data-cache-v1";
 const urlsToCache = [
-  "/Smart-Dashboard", // 預加載的頁面
-  "/static/js/bundle.js", // 預加載的 JavaScript
+  // "/Smart-Dashboard", // 預加載的頁面
+  // "/static/js/bundle.js", // 預加載的 JavaScript
 ];
 
 // 安裝階段，快取靜態資源
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      // return cache.addAll(urlsToCache);
-      return Promise.all([
-        cache.add("/").catch((error) => {
-          console.error("缓存 / 失败:", error);
-        }),
-        cache.add("/static/js/bundle.js").catch((error) => {
-          console.error("缓存 /static/js/bundle.js", error);
-        }),
-      ]);
+      return cache.addAll(urlsToCache);
     })
   );
 });
